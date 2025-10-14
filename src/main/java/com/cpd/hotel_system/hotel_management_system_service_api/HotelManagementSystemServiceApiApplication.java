63f11128dd293f0739b45376d41ca200c3705e94
Tharusha Kulasinghe <tharusha22589@gmail.com>
2025-10-14 05:38:25 +0530

    All the config files added

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_system_service_api/HotelManagementSystemServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_system.hotel_management_system_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemServiceApiApplication.class, args);
+	}
+
+}
