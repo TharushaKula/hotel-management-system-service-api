f643de5dc76808a7a8e5d1cbcd124f9ed9ea0523
Tharusha Kulasinghe <tharusha22589@gmail.com>
2025-10-14 05:49:08 +0530

    address added

--- src/main/java/com/cpd/hotel_system/hotel_management_system_service_api/entity/Hotel.java
+++ src/main/java/com/cpd/hotel_system/hotel_management_system_service_api/entity/Hotel.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Hotel {
 
     @Id
-    @Column(name = "hotel_id")
+    @Column(name = "hotel_id", length = 80)
     private String hotel_id;
 
     @Column(name = "hotel_name")
