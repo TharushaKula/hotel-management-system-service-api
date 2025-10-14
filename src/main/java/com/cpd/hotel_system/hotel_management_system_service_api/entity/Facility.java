964b3f3dfef6cd90784c802898513beb27961e32
Tharusha Kulasinghe <tharusha22589@gmail.com>
2025-10-14 21:25:15 +0530

    File Formatter added

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_system_service_api/entity/Facility.java
@@ -0,0 +1,14 @@
+package com.cpd.hotel_system.hotel_management_system_service_api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Facility")
+public class Facility {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 100, nullable = false, name = "name")
+    private String name;
+}
